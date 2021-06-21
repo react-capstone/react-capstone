487371f436ede98586f9c7fecd64ec553ff32dec
ivwu22 <ivywu6@gmail.com>
2021-06-21 11:06:36 -0400

    installs bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import React, { Component } from "react";
 import "./App.css";
 import Landing from "./pages/Landing/Landing";
 import Game from "./pages/Game/Game";
 import Summary from "./components/Summary/Summary";
 
-function App() {
-  return (
-    <>
-      <Landing />
-      <Game />
-      <Summary />
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Landing />
+        <Game />
+        <Summary />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Instructions/LandingLayOut/LandingLayOut.js
+++ src/components/Instructions/LandingLayOut/LandingLayOut.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 class LandingLayOut extends Component {
   render() {
-    return <div>LandingLayOut</div>;
+    return (
+      <div>
+        <h1>LandingLayOut</h1>
+        <Button>START</Button>
+      </div>
+    );
   }
 }
 
