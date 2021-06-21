cdca2edaee57b713b65a82ac8018c076f5d85f8d
Andre Brown <andreups@msn.com>
2021-06-21 17:34:41 -0400

    updated landing page border, background, and opacity

--- src/components/Instructions/LandingLayOut/LandingLayOut.js
+++ src/components/Instructions/LandingLayOut/LandingLayOut.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import Instructions from "../Instructions";
 import { Jumbotron, Button } from "react-bootstrap";
-import css from "./LandingLayOut.css";
-
+import "./LandingLayOut.css";
 
 class LandingLayOut extends Component {
   constructor(props) {
@@ -12,33 +11,36 @@ class LandingLayOut extends Component {
     };
   }
 
-
-
   render() {
     return (
-      <Jumbotron>
-        <div className="Border">
+      <>
+        <div className="border">
           <body className="landingPage">
-            <section >
+            <section>
               <h1 className="title">Triva Time</h1>
               <h3 className="instructionsTitle">Instructions</h3>
-              <p className="instructionsPara">-Vivamus eu nibh elementum, consequat massa vitae, venenatis ipsum.
+              <p className="instructionsPara">
+                -Vivamus eu nibh elementum, consequat massa vitae, venenatis
+                ipsum.
                 <br></br>
                 -Quisque faucibus erat hendrerit purus aliquet blandit.
                 <br></br>
                 -Nam vitae ipsum sed dui mattis efficitur nec a mi.
                 <br></br>
-                -Phasellus sed mi pretium, placerat odio sit amet, pulvinar dui.</p>
+                -Phasellus sed mi pretium, placerat odio sit amet, pulvinar dui.
+              </p>
               <form>
-                <label className="playerName" for="PlayerName">Player Name:</label>
-                <input type="text" name="Player Name:" />
+                <label className="playerName" for="playerName">
+                  Player Name:
+                </label>
+                <input className="nameField" type="text" name="Player Name:" />
               </form>
             </section>
             {/* <Instructions /> */}
             <Button>START</Button>
           </body>
-          </div>
-          </Jumbotron>
+        </div>
+      </>
     );
   }
 }
