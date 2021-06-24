41512df5f089bfa0bf32550c359f8ad9f7127f07
ivwu22 <ivywu6@gmail.com>
2021-06-24 09:59:56 -0400

    Updates

--- src/components/NoMatch/NoMatch.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class NoMatch extends Component {
-  render() {
-    return (
-      <h3>Sorry, there's nothing found at: {this.props.location.pathname}</h3>
-    );
-  }
-}
--- src/components/Points/Points.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Points extends Component {
-  render() {
-    return <div>Points</div>;
-  }
-}
-
-export default Points;
