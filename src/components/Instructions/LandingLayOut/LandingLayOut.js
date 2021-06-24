cf8214930e920406452b0cbb0a3fe2e638b84c1c
ivwu22 <ivywu6@gmail.com>
2021-06-24 15:55:27 -0400

    changes instructions format

--- src/components/Instructions/LandingLayOut/LandingLayOut.js
+++ src/components/Instructions/LandingLayOut/LandingLayOut.js
@@ -14,20 +14,20 @@ class LandingLayOut extends Component {
       <Jumbotron className="border jumbo">
         <div className="landingPage">
           <h1 className="title">Trivia Time</h1>
-          <h3 className="instructionsTitle">Instructions</h3>
+          {/* <h3 className="instructionsTitle">Instructions</h3> */}
           <span className="instructionsPara">
-            <ul className="ruleList">
-              <li>1000 points wins the game.</li>
-              <li>Each round has up to 15 questions.</li>
-              <li>Easy questions are worth 100 points.</li>
-              <li>Medium questions are worth 200 points.</li>
-              <li>Hard questions are worth 300 points.</li>
-              <li>Hints cost 50 points and display a helpful GIF.</li>
-              <li>
+            <span className="ruleList">
+              <div>1000 points wins the game.</div>
+              <div>Each round has up to 15 questions.</div>
+              <div>Easy questions are worth 100 points.</div>
+              <div>Medium questions are worth 200 points.</div>
+              <div>Hard questions are worth 300 points.</div>
+              <div>Hints cost 50 points and display a helpful GIF.</div>
+              <div>
                 If you have not scored 1000 points after question 15, you lose
                 the game.
-              </li>
-            </ul>
+              </div>
+            </span>
           </span>
           <form>
             <label className="playerName" for="playerName">
