d914aece35cd44a5e80badb2e91f1f7026f06443
Andre Brown <andreups@msn.com>
2021-06-25 11:32:19 -0400

    Updated NoMatch to working functional component

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,17 +1,16 @@
-import React from "react";
 import "./NoMatch.css";
 import { Jumbotron, Image } from "react-bootstrap";
 import nomatchmario from "../../Images/nomatchmario.gif";
+import { useLocation } from "react-router-dom";
 
-const NoMatch = () => {
+function NoMatch(props) {
+  const location = useLocation();
   return (
     <Jumbotron className="noMatchJumbo">
-      <h3 className="noMatchMsg">
-        No match for the path: {this.props.location.pathname}
-      </h3>
+      <h3 className="noMatchMsg">No match for the path: {location.pathname}</h3>
       <Image src={nomatchmario} alt="No page found" className="noMatchImg" />
     </Jumbotron>
   );
-};
+}
 
 export default NoMatch;
